bc8444e5867e19ff9cd77cce876817e34d55b5d9
bellaborrego38 <bellaborrego38@yahoo.com>
2023-07-19 18:17:05 -0500

    Added pie chart

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,26 @@
 import React from "react";
 import Main from "./components/main"
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Example from './components/pieChart';
+
 
 function App() {
     return (
+        <>
+        
+        <div className="pie-chart">
+
+      <Example />
+
+        </div>
+
         <div>
 
             <Main />
                 
         </div>
+
+        </>
     );
 }
 
