ede46382cc88cb9398bfd57d1c280ced7985d4a0
Rosa Alonso <alonso_2018@yahoo.com>
2023-07-19 17:49:20 -0500

    added login and register component

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,24 @@
 import React from "react";
-import Main from "./components/main"
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useState } from "react";
+import Main from "./components/main";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Register from "./components/register";
+import Login from "./components/login";
+
+// function App() {
+//   return (
+//     <div>
+//       <Main />
+//     </div>
+//   );
+// }
 
 function App() {
-    return (
-        <div>
-
-            <Main />
-                
-        </div>
-    );
+  //   const { currentForm, setCurrentForm } = useState("login");
+  return (
+    <div>
+      <Login />
+    </div>
+  );
 }
-
 export default App;
