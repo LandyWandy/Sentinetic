f77dc6e2bcc83503c027e790d516483d2dc643bc
Rosa Alonso <alonso_2018@yahoo.com>
2023-07-19 18:04:34 -0500

    commented out Login page on app.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,20 +5,20 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "./components/register";
 import Login from "./components/login";
 
-// function App() {
-//   return (
-//     <div>
-//       <Main />
-//     </div>
-//   );
-// }
-
 function App() {
-  //   const { currentForm, setCurrentForm } = useState("login");
   return (
     <div>
-      <Login />
+      <Main />
     </div>
   );
 }
+
+// function App() {
+//    const { currentForm, setCurrentForm } = useState("login");
+//   return (
+//     <div>
+//       <Login />
+//     </div>
+//   );
+// }
 export default App;
