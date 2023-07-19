869cacb121d270f541abf3d0fd19de714a0aad51
andrewr50 <51382718+andrewr50@users.noreply.github.com>
2023-07-18 20:22:12 -0500

    added some dependencies and change schema folder to graphql

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 
+const MONGODB_URI = 'mongodb+srv://Droobie:yelooD13@cluster0.9popvnh.mongodb.net/?retryWrites=true&w=majority'
+
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
