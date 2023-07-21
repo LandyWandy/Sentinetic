6dcee753082e5f4fd89d5ef2fd6c9ef1db4c607c
Rosa Alonso <alonso_2018@yahoo.com>
2023-07-20 20:57:37 -0500

    pushing current changes to login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
-import Main from "./components/main"
+import Main from "./components/main";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "./components/register";
 import Login from "./components/login";
-import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: 'http://localhost:3001/graphql',
+  uri: "http://localhost:3001/graphql",
   cache: new InMemoryCache(),
-})
+});
 
 function App() {
   return (
--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -1,7 +1,8 @@
 import React, {useState} from "react";
 import Register from './register'
-import '../css/style.css';
-import Main from "./main";
+// import '../css/style.css';
+// import Main from "./main";
+import '../css/login.css';
 
 // import { checkPassword, validateEmail } from '../utils/helpers';
 
@@ -35,11 +36,10 @@ function Login () {
     return (
         <section>
         <div className="form">
-            <div className="grid-container">
-                <div className="grid-item-row bg-light">
-                    <h1>Sentinetic</h1>
+            <div>
+                <div  >
+                    <h1 class="headerOne">Sentinetic</h1>
                 </div>
-
                 <div>
                     <div className="input-form-header">
                     <h3>Login</h3>
@@ -53,7 +53,7 @@ function Login () {
                                     onChange={(e) => setEmail(e.target.value)} 
                                     type="email" 
                                     className="form-control" 
-                                    placeholder="youremail@gmail.com" 
+                                    placeholder="Email" 
                                     aria-label="Email" 
                                     aria-describedby="basic-addon2" 
                                     id="email" name="email"
@@ -64,23 +64,24 @@ function Login () {
                                     value={password}   
                                     onChange={(e) => setPassword(e.target.value)} 
                                     type="password" 
-                                    placeholder="******" 
+                                    placeholder="Password" 
                                     aria-label="Password" 
                                     aria-describedby="basic-addon2"
                                     id="password" 
                                     name="password"/>
-                                <div className="input-group-append">
+
+                            </div>
+                            <div className="input-group-append" class="d-flex justify-content-center">
                                     <button 
-                                        className="btn btn-outline-secondary"
+                                        className="btn btn-outline-danger"
                                         type ="submit" 
                                         onSubmit={handleSubmit}
                                     >
                                         Log In
                                     </button>
-                                </div>
-                                <div>
-                                    <button className="btn btn-outline-secondary" onClick={Register}>Register here if you do not have an account </button>
-                                </div>
+                            </div>    
+                            <div class="text-center">
+                                <button className="btn btn-sm btn-block btn-danger" onClick={Register}>Register here if you do not have an account </button>
                             </div>
                         </form>
                     </div>
